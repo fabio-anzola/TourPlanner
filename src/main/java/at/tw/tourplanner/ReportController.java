0322cafb78e4d935af92f0a4a07579df85d7f903
Anzola Fabio <anzola.fabio@gmail.com>
2025-02-23 14:17:01 +0100

    Added controller from report windows

---
+++ src/main/java/at/tw/tourplanner/ReportController.java
@@ -0,0 +1,13 @@
+package at.tw.tourplanner;
+
+import javafx.event.ActionEvent;
+
+public class ReportController {
+    public void closeReport(ActionEvent actionEvent) {
+
+    }
+
+    public void closeSummary(ActionEvent actionEvent) {
+
+    }
+}
