eb596a8e0224fd8068fae2e6d77ee37e21afb43a
Anzola Fabio <anzola.fabio@gmail.com>
2025-03-14 11:42:14 +0100

    added module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module at.tw.tourplanner {
     requires javafx.controls;
     requires javafx.fxml;
     requires static lombok;
+    requires java.desktop;
 
 
     opens at.tw.tourplanner to javafx.fxml;
