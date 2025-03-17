8a71d36ca4760af9149c7ec89aa0da80a02bb639
Anzola Fabio <anzola.fabio@gmail.com>
2025-03-17 21:28:35 +0100

    Export objects to fx

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,10 @@ module at.tw.tourplanner {
     requires javafx.fxml;
     requires static lombok;
     requires java.desktop;
+A    requires jdk.jdi;
 
 
     opens at.tw.tourplanner to javafx.fxml;
+    opens at.tw.tourplanner.object to javafx.base;
     exports at.tw.tourplanner;
 }
