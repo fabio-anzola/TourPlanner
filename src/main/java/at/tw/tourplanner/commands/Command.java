0032cab96663c3bc54642722bf3b4d302132ea2a
Anzola Fabio <anzola.fabio@gmail.com>
2025-03-22 18:18:02 +0100

    Commented command pattern

--- src/main/java/at/tw/tourplanner/commands/Command.java
+++ src/main/java/at/tw/tourplanner/commands/Command.java
@@ -3,17 +3,34 @@ package at.tw.tourplanner.commands;
 import javafx.stage.Stage;
 import lombok.Getter;
 
+/**
+ * Command class as an example for the Command Design Pattern
+ */
 public abstract class Command {
 
+    /**
+     * The parent stage is used as the command level.
+     */
     @Getter
     private Stage parentStage;
 
+    /**
+     * Default Constructor with the parent stage
+     *
+     * @param parentStage
+     */
     public Command(Stage parentStage) {
         this.parentStage = parentStage;
     }
 
+    /**
+     * The inner execute method needs to be implemented by the command itself
+     */
     abstract void innerExecute();
 
+    /**
+     * The execute function handles the real executing. Here logging logic can be implemented.
+     */
     public void execute() {
         // Log logic
         innerExecute();
--- src/main/java/at/tw/tourplanner/commands/ExitCommand.java
+++ src/main/java/at/tw/tourplanner/commands/ExitCommand.java
@@ -2,12 +2,23 @@ package at.tw.tourplanner.commands;
 
 import javafx.stage.Stage;
 
+/**
+ * The exit command as an implementation of the command pattern
+ */
 public class ExitCommand extends Command {
 
+    /**
+     * Constructor of the Exit Command
+     *
+     * @param parentStage here the user can set the parent root stage
+     */
     public ExitCommand(Stage parentStage) {
         super(parentStage);
     }
 
+    /**
+     * Setup of the inner execute method
+     */
     @Override
     void innerExecute() {
         // Close stage
