6fa7a8ffab53f4eef8503a576a443f79d072efb7
Anzola Fabio <anzola.fabio@gmail.com>
2025-03-26 17:16:32 +0100

    added BE

---
+++ BE/src/main/java/at/tw/tourplanner/tourplanner/TourplannerApplication.java
@@ -0,0 +1,13 @@
+package at.tw.tourplanner.tourplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourplannerApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/at/tw/tourplanner/tourplanner/TourplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.tw.tourplanner.tourplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
