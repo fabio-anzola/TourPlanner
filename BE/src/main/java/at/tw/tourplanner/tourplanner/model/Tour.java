beb054f051517e7d6de94f92a43bda56b84c5c3c
Anzola Fabio <anzola.fabio@gmail.com>
2025-03-27 19:19:00 +0100

    added tour model

---
+++ BE/src/main/java/at/tw/tourplanner/tourplanner/model/Tour.java
@@ -0,0 +1,36 @@
+package at.tw.tourplanner.tourplanner.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Table(name = "tours")
+public class Tour {
+
+    @Id
+    @Column(name = "name")
+    @Getter
+    private String name;
+
+    @Column(name = "description")
+    @Getter
+    private String description;
+
+    @Column(name = "fromLocation")
+    @Getter
+    private String fromLocation;
+
+    @Column(name = "toLocation")
+    @Getter
+    private String toLocation;
+
+    public Tour(String name, String description, String fromLocation, String toLocation) {
+        this.name = name;
+        this.description = description;
+        this.fromLocation = fromLocation;
+        this.toLocation = toLocation;
+    }
+
+    public Tour() {
+    }
+}
