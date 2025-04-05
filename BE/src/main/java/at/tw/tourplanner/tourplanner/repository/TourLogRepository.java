7f630a99f85aac1dd4a1f503a9449fe0881993d9
Anzola Fabio <anzola.fabio@gmail.com>
2025-04-05 14:50:00 +0200

    added new get method for logs

--- BE/src/main/java/at/tw/tourplanner/tourplanner/repository/TourLogRepository.java
+++ BE/src/main/java/at/tw/tourplanner/tourplanner/repository/TourLogRepository.java
@@ -3,4 +3,8 @@ package at.tw.tourplanner.tourplanner.repository;
 import at.tw.tourplanner.tourplanner.model.TourLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TourLogRepository extends JpaRepository<TourLog, Long> { }
+import java.util.List;
+
+public interface TourLogRepository extends JpaRepository<TourLog, Long> {
+    List<TourLog> findByTourName(String tourName);
+}
