32ce13a2fe8ac72cfa3e188042dfab9b11d7753a
Anzola Fabio <anzola.fabio@gmail.com>
2025-04-24 12:32:57 +0200

    added route data object

---
+++ FE/src/main/java/at/tw/tourplanner/object/RouteData.java
@@ -0,0 +1,22 @@
+package at.tw.tourplanner.object;
+
+import lombok.Getter;
+
+@Getter
+public class RouteData {
+    public double distance;
+    public double duration;
+    public String geoJson;
+
+    public RouteData(double distance, double duration, String geoJson) {
+        this.distance = distance;
+        this.duration = duration;
+        this.geoJson = geoJson;
+    }
+
+    public RouteData(String[] res) {
+        this.distance = res[0] != null ? Double.parseDouble(res[0]) : 0;
+        this.duration = res[1] != null ? Double.parseDouble(res[1]) : 0;
+        this.geoJson = res[2];
+    }
+}
