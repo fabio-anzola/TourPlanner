e352d80203eb7ff1b12b83a3b5f781a852bef9f5
Anzola Fabio <anzola.fabio@gmail.com>
2025-05-01 18:49:12 +0200

    added transport type

--- FE/src/main/java/at/tw/tourplanner/dto/TourDto.java
+++ FE/src/main/java/at/tw/tourplanner/dto/TourDto.java
@@ -1,8 +1,11 @@
 package at.tw.tourplanner.dto;
 
+import at.tw.tourplanner.object.TransportType;
+
 public class TourDto {
     public String name;
     public String description;
     public String fromLocation;
     public String toLocation;
+    public String transportType;
 }
