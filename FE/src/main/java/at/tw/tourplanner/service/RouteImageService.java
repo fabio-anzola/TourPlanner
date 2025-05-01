d85e47b797725140bc1e04889e3ce3b9c2e87b70
Anzola Fabio <anzola.fabio@gmail.com>
2025-05-01 16:04:39 +0200

    import url from properties

--- FE/src/main/java/at/tw/tourplanner/service/RouteImageService.java
+++ FE/src/main/java/at/tw/tourplanner/service/RouteImageService.java
@@ -18,7 +18,7 @@ import javafx.scene.web.WebView;
 import java.io.FileWriter;
 
 public class RouteImageService {
-    private static final String BASE_URL = AppConfig.getBackendApiUrl();
+    private static final String BASE_URL = AppConfig.getBackendApiUrl() + "/api";
 
     public double[] geocode(String address) throws Exception {
         String encodedAddress = URLEncoder.encode(address, StandardCharsets.UTF_8);
