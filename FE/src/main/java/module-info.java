9a99db57830945ab81f143e07f6262fc1dec1fd9
Anzola Fabio <anzola.fabio@gmail.com>
2025-05-01 16:04:13 +0200

    added reqs

--- FE/src/main/java/module-info.java
+++ FE/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module at.tw.tourplanner {
     requires javafx.controls;
     requires javafx.fxml;
     requires static lombok;
-    requires java.desktop;
     requires jdk.jdi;
     requires org.json;
     requires org.kordamp.ikonli.javafx;
@@ -11,11 +10,16 @@ module at.tw.tourplanner {
     requires layout;
     requires javafx.web;
     requires org.apache.logging.log4j;
+    requires java.net.http;
+    requires com.fasterxml.jackson.databind;
+    requires java.sql;
+    requires spring.web;
 
 
     opens at.tw.tourplanner to javafx.fxml;
     opens at.tw.tourplanner.object to javafx.base;
     exports at.tw.tourplanner;
+    exports at.tw.tourplanner.dto to com.fasterxml.jackson.databind;
 
     exports at.tw.tourplanner.object;
 }
