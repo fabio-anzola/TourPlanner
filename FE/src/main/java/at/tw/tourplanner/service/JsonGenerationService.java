424b39246ecc21feae28d282b2762a349dc61adb
Anzola Fabio <anzola.fabio@gmail.com>
2025-05-23 11:34:03 +0200

    added docs to export service

--- FE/src/main/java/at/tw/tourplanner/service/JsonGenerationService.java
+++ FE/src/main/java/at/tw/tourplanner/service/JsonGenerationService.java
@@ -10,18 +10,28 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 
 import java.io.File;
 import java.io.IOException;
-
 import java.util.List;
 
+/**
+ * Service for generating tour JSON files.
+ */
 public class JsonGenerationService {
+    /** The output file for JSON data. */
     File file;
-    // log4j
+
+    /** The logger instance. */
     private static final ILoggerWrapper logger = LoggerFactory.getLogger(MainApplication.class);
 
+    /**
+     * Creates a JsonGenerationService for the given file.
+     */
     public JsonGenerationService(File file){
         this.file = file;
     }
 
+    /**
+     * Writes a list of tours to a JSON file.
+     */
     public void generateTourJson(List<Tour> tours) throws IOException {
         logger.debug("entered function: generateTourJson (JsonGenerationService) with parameter: " + tours);
         ObjectMapper mapper = new ObjectMapper();
