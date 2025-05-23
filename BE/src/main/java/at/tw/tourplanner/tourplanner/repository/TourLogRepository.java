b9a0f1a97ad5080f0f16b36cff529fc9161d417d
theladykiller <niki.plachetzky@gmail.com>
2025-05-23 11:12:08 +0200

    added javadoc to repositories

--- BE/src/main/java/at/tw/tourplanner/tourplanner/repository/TourLogRepository.java
+++ BE/src/main/java/at/tw/tourplanner/tourplanner/repository/TourLogRepository.java
@@ -5,6 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+/**
+ * Repository interface for accessing TourLog entities.
+ */
 public interface TourLogRepository extends JpaRepository<TourLog, Long> {
+    /**
+     * Finds all TourLogs associated with a given tour name.
+     *
+     * @param tourName the name of the tour
+     * @return a list of TourLogs belonging to the specified tour
+     */
     List<TourLog> findByTourName(String tourName);
 }
--- BE/src/main/java/at/tw/tourplanner/tourplanner/repository/TourRepository.java
+++ BE/src/main/java/at/tw/tourplanner/tourplanner/repository/TourRepository.java
@@ -3,4 +3,7 @@ package at.tw.tourplanner.tourplanner.repository;
 import at.tw.tourplanner.tourplanner.model.Tour;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for accessing Tour entities.
+ */
 public interface TourRepository extends JpaRepository<Tour, String> { }
