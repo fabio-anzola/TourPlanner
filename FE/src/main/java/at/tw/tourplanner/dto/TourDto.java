4ad5fbf31cf1f0b92ce76d140078a5020bc341dc
Anzola Fabio <anzola.fabio@gmail.com>
2025-05-23 11:32:39 +0200

    added docs to DTO Objects

--- FE/src/main/java/at/tw/tourplanner/dto/TourDto.java
+++ FE/src/main/java/at/tw/tourplanner/dto/TourDto.java
@@ -1,11 +1,21 @@
 package at.tw.tourplanner.dto;
 
-import at.tw.tourplanner.object.TransportType;
-
+/**
+ * Data transfer object for a tour.
+ */
 public class TourDto {
+    /** The name of the tour. */
     public String name;
+
+    /** The description of the tour. */
     public String description;
+
+    /** The starting location. */
     public String fromLocation;
+
+    /** The destination location. */
     public String toLocation;
+
+    /** The type of transport used. */
     public String transportType;
 }
--- FE/src/main/java/at/tw/tourplanner/dto/TourLogDto.java
+++ FE/src/main/java/at/tw/tourplanner/dto/TourLogDto.java
@@ -1,17 +1,34 @@
 package at.tw.tourplanner.dto;
 
 import lombok.Getter;
-
 import java.sql.Date;
 
+/**
+ * Data transfer object for a tour log.
+ */
 @Getter
 public class TourLogDto {
+    /** The ID of the tour log. */
     public long id;
+
+    /** The date of the log entry. */
     public Date date;
+
+    /** The comment for the tour log. */
     public String comment;
+
+    /** The difficulty rating. */
     public int difficulty;
+
+    /** The total distance of the tour. */
     public int totalDistance;
+
+    /** The total time of the tour. */
     public int totalTime;
+
+    /** The overall rating of the tour. */
     public int rating;
+
+    /** The tour associated with this log. */
     public TourDto tour;
 }
