c68271b3c7bd2fe9ce0850772db5e6405eafd4ec
theladykiller <niki.plachetzky@gmail.com>
2025-05-23 11:09:48 +0200

    added javadoc to transport mode

--- BE/src/main/java/at/tw/tourplanner/tourplanner/model/TransportMode.java
+++ BE/src/main/java/at/tw/tourplanner/tourplanner/model/TransportMode.java
@@ -1,5 +1,8 @@
 package at.tw.tourplanner.tourplanner.model;
 
+/**
+ * Enum representing different modes of transport.
+ */
 public enum TransportMode {
     CAR("driving-car"),
     BICYCLE("cycling-regular"),
@@ -8,14 +11,31 @@ public enum TransportMode {
 
     private final String orsProfile;
 
+    /**
+     * Constructor for TransportMode enum.
+     *
+     * @param orsProfile the ORS profile string for the mode
+     */
     TransportMode(String orsProfile) {
         this.orsProfile = orsProfile;
     }
 
+    /**
+     * Gets the ORS profile string.
+     *
+     * @return the ORS profile string
+     */
     public String getOrsProfile() {
         return orsProfile;
     }
 
+    /**
+     * Converts a string value to a corresponding TransportMode.
+     *
+     * @param value the string representation of the transport mode
+     * @return the matching TransportMode
+     * @throws IllegalArgumentException if the transport mode is unsupported
+     */
     public static TransportMode fromString(String value) {
         return switch (value.toLowerCase()) {
             case "car" -> CAR;
