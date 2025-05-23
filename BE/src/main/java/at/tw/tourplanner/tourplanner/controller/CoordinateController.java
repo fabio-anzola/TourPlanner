be2b55a91c84b394a4b6f68b6c487cda08e8fe69
theladykiller <niki.plachetzky@gmail.com>
2025-05-23 10:51:53 +0200

    added javadoc to coordinate controller

--- BE/src/main/java/at/tw/tourplanner/tourplanner/controller/CoordinateController.java
+++ BE/src/main/java/at/tw/tourplanner/tourplanner/controller/CoordinateController.java
@@ -9,16 +9,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * REST controller for geocoding addresses to geographic coordinates.
+ */
 @RestController
 @RequestMapping("/api/coordinates")
 public class CoordinateController {
 
     private final GeocodeService geocodeService;
 
+    /**
+     * Constructor that initializes the GeocodeService dependency.
+     *
+     * @param geocodeService the service used for geocoding operations
+     */
     public CoordinateController(GeocodeService geocodeService) {
         this.geocodeService = geocodeService;
     }
 
+    /**
+     * Endpoint for geocoding a given address into latitude and longitude.
+     *
+     * @param address the address to geocode
+     * @return ResponseEntity containing CoordinateDTO if successful, or appropriate error response
+     */
     @GetMapping
     public ResponseEntity<CoordinateDTO> geocode(@RequestParam String address) {
         try {
