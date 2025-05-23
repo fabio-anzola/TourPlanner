dbd6a394e908328f7fe119e0237c9701e7a18856
Anzola Fabio <anzola.fabio@gmail.com>
2025-05-23 11:32:26 +0200

    added docs to Logger factory

--- FE/src/main/java/at/tw/tourplanner/logger/LoggerFactory.java
+++ FE/src/main/java/at/tw/tourplanner/logger/LoggerFactory.java
@@ -1,6 +1,15 @@
 package at.tw.tourplanner.logger;
 
+/**
+ * Factory for creating logger wrappers.
+ */
 public class LoggerFactory {
+    /**
+     * Returns an initialized logger wrapper for the given class.
+     *
+     * @param clazz the class to associate with the logger
+     * @return an initialized ILoggerWrapper instance
+     */
     public static ILoggerWrapper getLogger(Class<?> clazz) {
         var logger = new Log4J2Wrapper(clazz);
         logger.initialize();
