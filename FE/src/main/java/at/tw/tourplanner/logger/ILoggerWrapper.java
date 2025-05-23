fc7954bd2335b5163db84772a2a696bc927ef2c8
Anzola Fabio <anzola.fabio@gmail.com>
2025-05-23 11:31:48 +0200

    added docs to Logging Wrapper

--- FE/src/main/java/at/tw/tourplanner/logger/ILoggerWrapper.java
+++ FE/src/main/java/at/tw/tourplanner/logger/ILoggerWrapper.java
@@ -1,9 +1,21 @@
 package at.tw.tourplanner.logger;
 
+/**
+ * Interface for logging messages at different levels.
+ */
 public interface ILoggerWrapper {
+    /** Logs a debug message. */
     void debug(String message);
+
+    /** Logs an info message. */
     void info(String message);
+
+    /** Logs a warning message. */
     void warn(String message);
+
+    /** Logs an error message. */
     void error(String message);
+
+    /** Logs a fatal error message. */
     void fatal(String message);
 }
