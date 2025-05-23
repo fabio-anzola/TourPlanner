f52533a1d1f3feb9831c53a45a62655aebfca7c4
theladykiller <niki.plachetzky@gmail.com>
2025-05-23 11:02:18 +0200

    added javadoc to coordinate dto

--- BE/src/main/java/at/tw/tourplanner/tourplanner/dto/CoordinateDTO.java
+++ BE/src/main/java/at/tw/tourplanner/tourplanner/dto/CoordinateDTO.java
@@ -1,17 +1,46 @@
 package at.tw.tourplanner.tourplanner.dto;
 
+/**
+ * Data Transfer Object representing geographical coordinates.
+ */
 public class CoordinateDTO {
     private double lon;
     private double lat;
 
+    /**
+     * Constructs a CoordinateDTO with specified longitude and latitude.
+     *
+     * @param lon longitude of the coordinate
+     * @param lat latitude of the coordinate
+     */
     public CoordinateDTO(double lon, double lat) {
         this.lon = lon;
         this.lat = lat;
     }
 
+    /**
+     * Gets the longitude.
+     *
+     * @return the longitude
+     */
     public double getLon() { return lon; }
+    /**
+     * Gets the latitude.
+     *
+     * @return the latitude
+     */
     public double getLat() { return lat; }
 
+    /**
+     * Sets the longitude.
+     *
+     * @param lon the longitude to set
+     */
     public void setLon(double lon) { this.lon = lon; }
+    /**
+     * Sets the latitude.
+     *
+     * @param lat the latitude to set
+     */
     public void setLat(double lat) { this.lat = lat; }
 }
